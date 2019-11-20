a4637ffbeafedcb7fe702f44908451013f53499f
jordiugarte <jordi933@gmail.com>
2019-11-20 15:32:41 -0400

    Authentication

--- app/src/main/java/com/digitalstring/projectjoby/MainActivity.java
+++ app/src/main/java/com/digitalstring/projectjoby/MainActivity.java
@@ -2,6 +2,7 @@ package com.digitalstring.projectjoby;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void register ( View view ){
+
+    }
+
+    public void log_in ( View view ){
+
     }
 }
